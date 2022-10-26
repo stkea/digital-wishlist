dace9e38a0d0b9c0b5ac83fbc6af6b9b490e6925
Cannabis2013 <MH2012@outlook.dk>
2022-10-26 16:08:25 +0200

    Reduced sql scripts to two files

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -29,16 +29,3 @@ CREATE TABLE Wish(
 
 CREATE INDEX idx_wid
     ON Wish(Id);
-USE KEA_Wishlist;
-
-CREATE VIEW wishes
-AS
-SELECT
-    Name,
-    Price,
-    Reserved,
-    Url
-FROM
-    Wish
-        INNER JOIN
-    Product ON Wish.Id = Product.WishId;
--- sql/make_wishlist.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Wishlist (Name,Creator,Expiration)
-VALUES("Elliots ønsker", "Thomas hansen","2023-08-14");
