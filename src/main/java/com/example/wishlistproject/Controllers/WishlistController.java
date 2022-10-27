e52d08b1a28dfc3b3142641f622dcf79300b3f76
stkea <stef240p@stud.kea.dk>
2022-10-27 19:59:41 +0200

    frontend started

--- src/main/java/com/example/wishlistproject/Controllers/WishlistController.java
+++ src/main/java/com/example/wishlistproject/Controllers/WishlistController.java
@@ -33,18 +33,10 @@ public class WishlistController {
         return "redirect:err";
     }
 
-    @GetMapping("/wishlist/remove")
-    public String removeGet(@RequestParam(value = "id") String id, Model model){
-        var wl = dbManager.getWishlistById(id);
-        if(wl == null)
-            return "err";
-        model.addAttribute("wishlist",wl);
-        return "removeConfirmation";
-    }
 
     @PostMapping("/wishlist/remove")
-    public String removePost(@ModelAttribute("wishlist") Wishlist wl){
-        var result = dbManager.removeWishlistById(wl.getId());
+    public String removePost(@RequestParam(value = "id") String id){
+        var result = dbManager.removeWishlistById(id);
         if(result)
             return "redirect:/wishlists";
         return "err";
