cb2052b5289d817c3f343528280116c942babc64
Cannabis2013 <MH2012@outlook.dk>
2022-11-03 12:48:11 +0100

    Removed launcher folder

--- launcher/launcher.sh
+++
@@ -1,5 +0,0 @@
-export ConStr=jdbc:mysql://localhost:3306/KEA_Wishlist
-./dbcredentials.sh
-cd ..
-mvn spring-boot:run
-
--- src/main/java/com/example/wishlistproject/Controllers/ShareController.java
+++ src/main/java/com/example/wishlistproject/Controllers/ShareController.java
@@ -30,7 +30,7 @@ public class ShareController {
         return "/Wishlist/sharelink";
     }
 
-    @GetMapping("share/{tokenKey}")
+    @GetMapping(" ")
     public String handleToken(@PathVariable String tokenKey){
         if(!tokenFactory.validateKey(tokenKey))
             return "redirect:/invalid_share_key/" + tokenKey;
